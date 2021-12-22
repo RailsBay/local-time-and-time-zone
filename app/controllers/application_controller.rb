3991df75842a6d407c452dc7755d6fad1eb7c007
printfinn <git@printf.win>
2021-12-22 21:41:31 +0800

    Local Time and Time Zone in Rails (part 2)
    Add a time zone attribute to user model, and pre populate time select with user's local time.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if:  :devise_controller?
+  around_action :set_time_zone, if: :current_user
+
+  private
+  def set_time_zone
+    Time.use_zone(current_user.time_zone) { yield }
+  end
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :time_zone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :time_zone])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_115538) do
+ActiveRecord::Schema.define(version: 2021_12_22_111412) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_12_21_115538) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
